d6d1f6b15e40d6974cfbed2469cd6d6b6e9a6e91
jeehong.lee <jeehong.lee@linecorp.com>
2016-08-10 15:25:25 +0900

    [NO BTS] Hello world

--- test.example/Tajan.go
+++ test.example/Tajan.go
@@ -6,7 +6,6 @@ func shout(singCount int) {
 	for i := 1; i <= singCount; i++ {
 		pants := i * 10
 		sword := pants + 10
-
 		fmt.Printf("타잔이 %d원짜리 팬티를 입고 %d원짜리 칼을 차고 노래를 한다.\r\n", pants, sword)
 	}
 }
